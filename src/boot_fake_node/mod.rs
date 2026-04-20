f9eb1dbc8222a3feecb5ee305747e22b8efb3c1c
skaunov <skaunov@disroot.org>
2026-04-20 23:44:20 +0300

    actually while fetching the thing it can get the local ones just in case

--- src/boot_fake_node/mod.rs
+++ src/boot_fake_node/mod.rs
@@ -139,38 +139,38 @@ pub fn get_platform_runtime_name(is_simulation_mode: bool) -> Result<String> {
     if !uname.status.success() {
         return Err(eyre!("Could not determine OS."));
     }
-    let os_name = std::str::from_utf8(&uname.stdout)?.trim();
+        let os_name = std::str::from_utf8(&uname.stdout)?.trim();
 
-    let uname_m = Command::new("uname").arg("-m").output()?;
+        let uname_m = Command::new("uname").arg("-m").output()?;
     if !uname_m.status.success() {
         return Err(eyre!("Could not determine architecture."));
     }
-    let architecture_name = std::str::from_utf8(&uname_m.stdout)?.trim();
+            let architecture_name = std::str::from_utf8(&uname_m.stdout)?.trim();
 
-    // TODO: update when have binaries
+                // TODO: update when have binaries
     let zip_name_midfix = match (os_name, architecture_name) {
-        ("Linux", "x86_64") => "x86_64-unknown-linux-gnu",
-        ("Linux", "aarch64") => "aarch64-unknown-linux-gnu",
-        ("Darwin", "arm64") => "arm64-apple-darwin",
-        ("Darwin", "x86_64") => "x86_64-apple-darwin",
-        _ => {
-            return Err(eyre!(
-                "OS/Architecture {}/{} not amongst pre-built [Linux/x86_64, Linux/aarch64, Apple/arm64, Apple/x86_64].",
-                os_name,
-                architecture_name,
-            ).with_suggestion(|| "Use the `--runtime-path` flag to build a local copy of the https://github.com/hyperware-ai/hyperdrive repo")
-            );
-        }
+                    ("Linux", "x86_64") => "x86_64-unknown-linux-gnu",
+                    ("Linux", "aarch64") => "aarch64-unknown-linux-gnu",
+                    ("Darwin", "arm64") => "arm64-apple-darwin",
+                    ("Darwin", "x86_64") => "x86_64-apple-darwin",
+                    _ => {
+                        return Err(eyre!(
+                            "OS/Architecture {}/{} not amongst pre-built [Linux/x86_64, Linux/aarch64, Apple/arm64, Apple/x86_64].",
+                            os_name,
+                            architecture_name,
+                        ).with_suggestion(|| "Use the `--runtime-path` flag to build a local copy of the https://github.com/hyperware-ai/hyperdrive repo")
+                        );
+                    }
     };
     Ok(format!(
         "hyperdrive-{}{}.zip",
         zip_name_midfix,
-        if is_simulation_mode {
-            "-simulation-mode"
-        } else {
-            ""
-        },
-    ))
+                if is_simulation_mode {
+                    "-simulation-mode"
+                } else {
+                    ""
+                },
+            ))
 }
 
 #[instrument(level = "trace", skip_all)]
@@ -180,11 +180,7 @@ async fn get_runtime_binary(version: &str, is_simulation_mode: bool) -> Result<P
     let version = if version != "latest" {
         version.to_string()
     } else {
-        find_releases_with_asset_if_online(
-            Some(HYPERWARE_OWNER),
-            Some(HYPERDRIVE_REPO),
-            &get_platform_runtime_name(is_simulation_mode)?,
-        ).await?
+        find_releases_with_asset_if_online(get_platform_runtime_name(is_simulation_mode)?).await?
         .first()
         .ok_or(eyre!("No releases found"))?
         .clone()
@@ -316,7 +312,7 @@ pub async fn fetch_releases(owner: &str, repo: &str) -> Result<Vec<Release>> {
 pub async fn find_releases_with_asset(
     owner: Option<&str>,
     repo: Option<&str>,
-    asset_name: &str,
+    asset_name: String,
 ) -> Result<Vec<String>> {
     let owner = owner.unwrap_or(HYPERWARE_OWNER);
     let repo = repo.unwrap_or(HYPERDRIVE_REPO);
@@ -333,19 +329,20 @@ pub async fn find_releases_with_asset(
     filtered_releases
 }
 
-pub async fn find_releases_with_asset_if_online(
-    owner: Option<&str>,
-    repo: Option<&str>,
-    asset_name: &str,
-) -> Result<Vec<String>> {
-    let remote_values = find_releases_with_asset(owner, repo, asset_name).await;
-    if remote_values.iter().flatten().next().is_none() {
-        tracing::debug!["nothing useful from remote fetching"];
-        let r = get_local_versions_with_prefix(&format!("{}v", LOCAL_PREFIX));
-        tracing::trace!("Using local versions. {r:?}");
-        return r.map(|versions| versions.iter()
-        .map(|v| format!("v{}", v)).collect())
-    } else {remote_values}
+pub async fn find_releases_with_asset_if_online(asset_name: String) -> Result<Vec<String>> {
+    let values_remote = tokio::spawn(find_releases_with_asset(
+        None, None, asset_name
+    ));
+    let values_local = get_local_versions_with_prefix(&format!("{}v", LOCAL_PREFIX));
+    tracing::trace!("Got local versions result. {values_local:?}");
+    let values_local = values_local.map(
+        |versions| versions.iter().map(|v| format!("v{}", v)).collect()
+    );
+    let values_remote = values_remote.await?;
+    if values_remote.iter().flatten().next().is_none() {
+        tracing::debug!("nothing useful from remote fetching, falling back to the local versions");
+        values_local
+    } else {values_remote}
 }
 
 #[instrument(level = "trace", skip_all)]
